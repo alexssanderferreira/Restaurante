9e119ac10b2615c3bb06c3af4d6458b55ccb04fe
Alexssander <alexssanderferreira@hotmail.com>
2024-11-03 20:10:15 -0300

    Corrigindo ItemDoMenu

--- Restaurante.Domain/Entidades/ItemDoMenu.cs
+++ Restaurante.Domain/Entidades/ItemDoMenu.cs
@@ -44,15 +44,9 @@ public class ItemDoMenu : Base
 
     private void ValidarItem(string nome, string descricao, double preco, string imagem)
     {
-        if (string.IsNullOrEmpty(nome))
-            _erros.Add("Nome não pode ser nulo ou vazio");
-        if (Nome.Length > TAMANHO_MAXIMO_NOME || Nome.Length < TAMANHO_MINIMO)
+        if (nome.Length > TAMANHO_MAXIMO_NOME || Nome.Length < TAMANHO_MINIMO)
             _erros.Add($"Nome não pode ser maior que {TAMANHO_MAXIMO_NOME} caracteres e nem menor que {TAMANHO_MINIMO}.");
-        if (string.IsNullOrEmpty(descricao))
-            _erros.Add("Descrição não pode ser nulo ou vazio");
-        if (string.IsNullOrEmpty(descricao))
-            _erros.Add("Nome não pode ser nulo ou vazio");
-        if (Descricao.Length > TAMANHO_MAXIMO_DESCRICAO || Descricao.Length < TAMANHO_MINIMO)
+        if (descricao.Length > TAMANHO_MAXIMO_DESCRICAO || Descricao.Length < TAMANHO_MINIMO)
             _erros.Add($"Nome não pode ser maior que {TAMANHO_MAXIMO_NOME} caracteres e nem menor que {TAMANHO_MINIMO}.");
         if (preco <= 0)
             _erros.Add("Preço não pode ser menor ou igual a zero");
